55dab5cb65ec9877901eb94753a1a113a4c0cac1
Jiacheng Yang <33099087+yjc765@users.noreply.github.com>
2019-04-15 19:06:52 +0200

    Update Pitasc_intallation_test.sh

--- Pitasc_intallation_test.sh
+++ Pitasc_intallation_test.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+cd ~./pitasc
+tar xf pitasc_2_5_0.tar.gz
+tar xf pitasc_common.tar.gz   # extract zip file 'pitasc_2_5_0.tar.gz' and get folder 'install' and pitasc_common
 
+sudo apt install python-catkin-tools 
+
+cd ~/pitasc
+rosdep install --from-path install/ -i -y           # install dependencies
+
+source install/setup.bash
+
+terminator -e 'roslaunch pitasc_common bringup_examples.launch' & terminator -e "sleep 5 && rosrun pitasc file_reader.py pitasc_common examples/beginner/simple_move.xml"
