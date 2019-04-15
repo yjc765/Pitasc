4212ca1d6882a3fcc7b6d7952f0e1963c8ff47ad
Jiacheng Yang <33099087+yjc765@users.noreply.github.com>
2019-04-15 19:12:02 +0200

    Update pitasc_installation.sh

--- pitasc_installation.sh
+++ pitasc_installation.sh
@@ -1,19 +1,18 @@
-$ cd ~/catkin_ws
-$ rm .catkin_workspace
-$ rm src/CMakeLists.txt
-$ rm -rf build/ devel/ install/
+#!/bin/bash
+cd ~/catkin_ws
+rm .catkin_workspace
+rm src/CMakeLists.txt
+rm -rf build/ devel/ install/
 
 # $ source /opt/ros/kinetic/setup.bash                  # automatically done by sourcing the /pitasc/setup.dash
-$ source ~/pitasc/install/setup.bash  
+source ~/pitasc/install/setup.bash  
 
-$ catkin init
-$ catkin build
+catkin init
+catkin build
 
 # move the extracted pitasc_commom.tar.gz folder to catkin_ws/src
+mv ~/pitasc/install ~/catkin_ws/src
 
-$ source ~/catkin_ws/devel/setup.bash
-$ roslaunch pitasc_common bringup_examples.launch 
+source ~/catkin_ws/devel/setup.bash
 
-# in a new terminal
-$ source ~/catkin_ws/devel/setup.bash
-$ rosrun pitasc file_reader.py pitasc_common examples/beginner/simple_move.xml
+terminator -e 'roslaunch pitasc_common bringup_examples.launch' & terminator -e "sleep 5 && rosrun pitasc file_reader.py pitasc_common examples/beginner/simple_move.xml"
